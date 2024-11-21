8615d995ecea29ae3253ebf90232ce44ffc55349
prudhvi <tpreddy005@gmail.com>
2024-11-21 17:06:33 +0530

    Adding project structure

---
+++ VendorApi/src/test/java/com/vendor/VendorApi/VendorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendor.VendorApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
